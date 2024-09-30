0bb458740935d3c1b80b2ff1dc67acd34d1b3544
TasleemHAS <153951478+TasleemHAS@users.noreply.github.com>
2024-09-30 20:16:26 +0530

    Update Area.java

--- Area.java
+++ Area.java
@@ -1,11 +1,22 @@
-public class Area
- {
-    public static void main(String[] args)
- {
-        double pi = 3.1416; 
+public class AreaCalculator {
+    private double radius;
+    private final double pi = 3.1416;
+
+    public AreaCalculator(double radius) {
+        this.radius = radius;
+    }
+
+    public double calculateArea() {
+        return pi * Math.pow(radius, 2);
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
         System.out.print("Enter the radius of the circle: ");
-        double r = Double.parseDouble(System.console().readLine());
-        double a = pi * Math.pow(r, 2); 
-        System.out.println("Area of circle is " + a);
+        double radius = Double.parseDouble(System.console().readLine());
+        AreaCalculator calculator = new AreaCalculator(radius);
+        double area = calculator.calculateArea();
+        System.out.println("The area of the circle is " + area);
     }
 }
